b57ba4db18df2b3a739b26b10cd9cba2797ac0d5
John Bien-aime <20131256@ce.pucmm.edu.do>
2019-05-31 00:45:48 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,13 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  counter = 0
+  loop do
+    turn(board)
+    if counter > 9
+      break
+    end
+  counter += 1
+  end
+end
